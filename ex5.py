f54c941b6616036c1904e1df0adc8c9beddf65d3
Vinicius Pimentel <61170319+Vinicius-Pimentel@users.noreply.github.com>
2021-05-30 19:44:27 -0300

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+def imagem1(m):
+    width = len(m[0])
+    height = len(m)
+    resposta=[[0 for x in range(height)] for y in range(width)]
+    for i in range(width):
+        for j in range(height):
+            teto = width-i
+            resposta[i][j] = m[j][teto-1]
+    return np.array(resposta)
+
+
+img = cv.imread('img/unifafibe.jpg')
+b,g,r = cv.split(img)
+b1 = imagem1(b)
+g1 = imagem1(g)
+r1 = imagem1(r)
+img2 = cv.merge((b1,g1,r1))
+plt.subplot(121),plt.imshow(img, cmap="gray"),plt.title('Original')
+plt.subplot(122),plt.imshow(img2, cmap="gray"),plt.title('Processed')
+plt.show()
+
+#height muda de cima para baixo e vice versa, o widght muda de esquerda para direita, e vice versa
